b17d2e470f6320f636266ba24c097f82d88b14ee
admin <viewsonic12380@gmail.com>
2020-07-25 11:10:43 +0500

    add fullName

--- src/main/java/model/Resume.java
+++ src/main/java/model/Resume.java
@@ -9,21 +9,20 @@ import java.util.UUID;
 public class Resume implements Comparable<Resume>{
     // Unique identifier
     private final String uuid;
+    private final String fullName;
 
-    public Resume() {
-        this(UUID.randomUUID().toString());
+    public Resume(String fullName) {
+        this(UUID.randomUUID().toString(),fullName);
     }
 
-    public Resume(String uuid) {
+    public Resume(String uuid,String fullName) {
+        Objects.requireNonNull(uuid,"uuid must not be null");
+        Objects.requireNonNull(fullName,"fullName must not be null");
         this.uuid = uuid;
+        this.fullName=fullName;
     }
 
     public String getUuid() {
-        return uuid;
-    }
-
-    @Override
-    public String toString() {
         return uuid;
     }
 
@@ -32,16 +31,28 @@ public class Resume implements Comparable<Resume>{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Resume resume = (Resume) o;
-        return Objects.equals(uuid, resume.uuid);
+        if(!uuid.equals(resume.uuid)) return false;
+        return fullName.equals(resume.fullName);
+//        return Objects.equals(uuid, resume.uuid);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uuid);
+//        return Objects.hash(uuid);
+        int result=uuid.hashCode();
+        result=31*result+fullName.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return uuid+'('+fullName+')';
     }
 
     @Override
     public int compareTo(Resume o) {
-        return uuid.compareTo(o.uuid);
+//        return uuid.compareTo(o.uuid);
+        int cmp=fullName.compareTo(o.fullName);
+        return cmp!=0?cmp:uuid.compareTo(o.uuid);
     }
 }
