b0b73b7983966654d7e0a317a9d0582a68f30ddb
admin <viewsonic12380@gmail.com>
2020-07-25 14:56:27 +0500

    modify MapUuidStorage and create MapResumeStorage

--- src/main/java/storage/MapUuidStorage.java
+++ src/main/java/storage/MapUuidStorage.java
@@ -13,41 +13,42 @@ public class MapUuidStorage extends AbstractStorage{
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchKey) {
+    protected void doUpdate(Resume r, Object uuid) {
+        map.put((String)uuid,r);
     }
 
     @Override
-    protected boolean isExist(Object searchKey) {
-        return false;
+    protected boolean isExist(Object uuid) {
+        return map.containsKey((String)uuid);
     }
 
     @Override
-    protected void doSave(Resume r, Object searchKey) {
-
+    protected void doSave(Resume r, Object uuid) {
+        map.put((String)uuid,r);
     }
 
     @Override
-    protected Resume doGet(Object searchKey) {
-        return null;
+    protected Resume doGet(Object uuid) {
+        return map.get((String)uuid);
     }
 
     @Override
-    protected void doDelete(Object searchKey) {
-
+    protected void doDelete(Object uuid) {
+        map.remove((String)uuid);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
     protected List<Resume> doCopyAll() {
-        return Collections.emptyList();
+        return new ArrayList<>(map.values());
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 }
