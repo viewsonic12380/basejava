a5676e866957c33713fa50845a048e69fefa57a6
admin <viewsonic12380@gmail.com>
2020-08-24 21:32:01 +0500

    HW12

---
+++ src/main/java/MyMainMinStream.java
@@ -0,0 +1,10 @@
+public class MyMainMinStream {
+    public static void main(String[] args) {
+        int[] arr1={1,2,3,3,2,3};// вернет 123, а
+        int[] arr2={9,8};        // вернет 89
+    }
+    public int minValue(int[] values){
+        
+        return 0;
+    }
+}
